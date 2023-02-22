f9d34dd60719bab5268c90992206bb7e67994846
MuhammadEgaRam <2141720143@student.polinema.ac.id>
2023-02-22 10:34:06 +0700

    Prak 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function about (){
+        echo 'MuhammadEgaRamaFernanda_2141720143';
+    }
+}
